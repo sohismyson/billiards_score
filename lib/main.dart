907f5e7983fa07b7c62d729e76fcf66c51ed351d
sohismyson <sohismyson@gmail.com>
2020-07-22 12:32:42 +0900

    8以上カウントできないように修正

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BILLIARDS SCORE BOARD',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'BILLIARDS SCORE BOARD'),
@@ -56,61 +43,74 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _scoreReseter() {
+    setState(() {
+      _homePlayerCounter = 0;
+      _guestPlayerCounter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        backgroundColor: Color(0xFF000000),
       ),
       body: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            GestureDetector(
-              onTap: _homePlayerIncrementCounter,
-              child: Container(
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  child: Text(
-                    '$_homePlayerCounter',
-                    style: TextStyle(
-                      color: Color(0xFFFFFFFF),
-                      fontWeight: FontWeight.w600,
+            Expanded(
+              flex: 5,
+              child: GestureDetector(
+                onTap: _homePlayerCounter != 7
+                    ? _homePlayerIncrementCounter
+                    : () {},
+                child: Container(
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text(
+                      '$_homePlayerCounter',
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
+                  color: Color(0xFF3C3C3C),
                 ),
-                color: Color(0xFF3C3C3C),
-                width: 48.0,
-                height: 48.0,
               ),
             ),
-            GestureDetector(
-              onTap: _guestPlayerIncrementCounter,
-              child: Container(
-                child: FittedBox(
-                  fit: BoxFit.contain,
-                  child: Text(
-                    '$_guestPlayerCounter',
-                    style: TextStyle(
-                      color: Color(0xFFFFFFFF),
-                      fontWeight: FontWeight.w600,
+            Expanded(
+              flex: 5,
+              child: GestureDetector(
+                onTap: _guestPlayerCounter != 7
+                    ? _guestPlayerIncrementCounter
+                    : () {},
+                child: Container(
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text(
+                      '$_guestPlayerCounter',
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
+                  color: Color(0xFF3C3C3C),
                 ),
-                color: Color(0xFF3C3C3C),
-                width: 48.0,
-                height: 48.0,
               ),
             ),
           ],
         ),
       ),
-/*       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+      floatingActionButton: FloatingActionButton(
+        onPressed: _scoreReseter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
- */
     );
   }
 }
